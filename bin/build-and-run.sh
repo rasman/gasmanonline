560d22042d2a30f8f1b4dd58b368ed40d4a65cb2
coolbrd <ite5011@psu.edu>
2022-11-04 14:32:57 -0400

    Updated build scripts to be a little less dangerous

--- bin/build-and-run.sh
+++ bin/build-and-run.sh
@@ -1,5 +1,7 @@
-$QT5_REPO/qt5-build/qtbase/bin/qmake $GASMAN_REPO/src/gasman.pro
+rm -rf ./build
 
-emmake make -j4
+mkdir build
+
+(cd build;$QT5_REPO/qtbase/bin/qmake $GASMAN_REPO/src/gasman.pro;emmake make -j4)
 
 $GASMAN_REPO/bin/gasmanonline-run.sh
